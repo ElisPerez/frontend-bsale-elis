55c73d8ca9990a8d52f48e1a7b394ca736eab421
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-25 22:01:28 -0500

    fo

--- docs/js/footer.js
+++ docs/js/footer.js
@@ -1,12 +1,9 @@
-/* IIFEs - Immediately Invoked Function Expressions. */
-(function () {
-  const footerID = document.querySelector('#footer-id');
+const footerID = document.querySelector('#footer-id');
 
-  /* Creating the footer with current Year and a Link to instagram. */
-  footerID.innerHTML = `
+/* Creating the footer with current Year and a Link to instagram. */
+footerID.innerHTML = `
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © ${new Date().getFullYear()} Copyright:
         <a class="text-dark" href="https://instagram.com/elisperezmusic" style="text-decoration-line: none;">Elis Antonio Perez</a>
       </div>
       `;
-})();
