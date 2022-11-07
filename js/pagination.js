d6339bdde06312407232a71d225067473456283c
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-11-06 23:23:15 -0500

    pagination 3

--- js/pagination.js
+++ js/pagination.js
@@ -4,63 +4,18 @@ const divContainerCards = $('#div-container-cards');
 const container = document.querySelector('#container');
 const btnSearch = document.querySelector('#idbutton');
 
-// export const pagination = url => {
-//   divPagination.pagination({
-//     dataSource: url, // Like this: http://localhost:8080/api/products/category?id=1&pageSize=4&pageNumber=1&_=1667774822605 OR like this: http://localhost:8080/api/products/?pageSize=4&pageNumber=1&_=1667774822605
-//     locator: 'products', // products: array fron response.
-//     totalNumberLocator: function (response) {
-//       // console.log('Response:', response.totalRows);
-//       return response.totalRows;
-//     },
-//     pageSize: 4,
-//     ajax: {
-//       beforeSend: function () {
-//         /* Showing a loading spinner while the data is being fetched from the API. */
-//         divContainerCards.html(`<div class="d-flex justify-content-center">
-//         <div class="m-5 p-5 d-flex align-content-center spinner-border text-secondary" role="status">
-//         <span class="visually-hidden">Loading...</span>
-//       </div>
-//         </div>`);
-//       },
-//     },
-//     callback: function (data, pagination) {
-//       // console.log('data:', data);
-//       // console.log('pagination:', pagination);
-//       /* Creating a variable called dataHTML and Add the opening div tag to it. */
-//       let dataHTML = '<div class="row align-items-center d-flex justify-content-center">';
-
-//       /* jQuery function to assign all cards to the dataHTML variable */
-//       $.each(data, function (index, product) {
-//         dataHTML += `
-//           <div class="col col-xs-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
-//             <div class="card" style="width: 14rem; height: 20rem">
-//               <img
-//                 src="${product.url_image ? product.url_image : '/img/no-image.jpg'}"
-//                 class="card-img-top"
-//                 alt="${product.name}"
-//                 style="width: auto; height: 240px"
-//               />
-//               <div class="card-body">
-//                 <h6 class="card-title">${product.name}</h6>
-//                 <p class="card-title">Price: $${product.price}</p>
-//               </div>
-//             </div>
-//           </div>`;
-//       });
-
-//       /* Adding the closing div tag to the variable dataHTML. */
-//       dataHTML += '</div>';
-
-//       /* Replacing the content of the div with the id of 'div-container-cards' with the content of the
-//       variable dataHTML. */
-//       divContainerCards.html(dataHTML);
-//     },
-//   });
-// };
-
+/** url like this:
+ * Get All Products:        http://localhost:8080/api/products?pageSize=4&pageNumber=1
+ * Get Product by Category: http://localhost:8080/api/products/category?id=1&pageSize=4&pageNumber=1
+ * Search:                  http://localhost:8080/api/products?q=something&pageSize=4&pageNumber=1
+ */
+/**
+ * It's a jQuery plugin that allows you to create a pagination
+ * @returns the value of the variable dataHTML.
+ */
 export const pagination = url => {
   divPagination.pagination({
-    dataSource: url, // Like this: http://localhost:8080/api/products/category?id=1&pageSize=4&pageNumber=1&_=1667774822605 OR like this: http://localhost:8080/api/products?pageSize=4&pageNumber=1&_=1667774822605 OR like this: http://localhost:8080/api/products?q=something&pageSize=4&pageNumber=1&_=1667774822605
+    dataSource: url,
     locator: 'products', // products: array fron response.
     totalNumberLocator: function (response) {
       // console.log('Response:', response.totalRows);
@@ -80,7 +35,6 @@ export const pagination = url => {
     callback: function (data, pagination) {
       // console.log('data:', data);
       // console.log('pagination:', pagination);
-      /* Creating a variable called dataHTML and Add the opening div tag to it. */
 
       if (data.length === 0) {
         /* Cleaning the container and displaying a message saying that the product was not found. */
@@ -96,6 +50,7 @@ export const pagination = url => {
         /* Cleaning the container. */
         container.innerHTML = '';
 
+        /* Creating a variable called dataHTML and Add the opening div tag to it. */
         let dataHTML = '<div class="row align-items-center d-flex justify-content-center">';
 
         /* jQuery function to assign all cards to the dataHTML variable */
--- js/search.js
+++ js/search.js
@@ -1,16 +1,13 @@
-import { fetchAPI } from '../api/fetchAPI.js';
 import { pagination } from './pagination.js';
 
-const pageName = document.querySelector('#page-name');
 const container = document.querySelector('#container');
+const pageName = document.querySelector('#page-name');
 const btnSearch = document.querySelector('#idbutton');
-
-const divContainerCards = $('#div-container-cards');
-const divPagination = $('#pagination-container');
 
 /* Search: function that is being called when the form is being submitted. */
 form.onsubmit = async function (e) {
   e.preventDefault();
+  container.innerHTML = ``;
 
   /* Disabling the button so that the user cannot click on it while the data is being fetched. */
   btnSearch.disabled = true;
@@ -26,63 +23,6 @@ form.onsubmit = async function (e) {
   /* Changing the name of the page (h1). */
   pageName.innerHTML = `Search`;
 
-  /* Get products: Fetching the data from the API and returning products as a JSON object. */
-  // const products = await fetchAPI('/products/search' + `?q=${search}`);
-
-  pagination(`http://localhost:8080/api/products/search?q=${search}`)
-
-  /* Checking if the products array is empty. If it is empty, it is displaying a message saying that the product was not found. If it is not empty, it is cleaning the container and displaying the products. */
-  // if (products.length === 0) {
-  //   /* Cleaning the container and displaying a message saying that the product was not found. */
-  //   container.innerHTML = `<div class="col mb-3 d-flex justify-content-center"><h2>Producto No Encontrado</h2></div>`;
-
-  //   /* Cleaning the container and the pagination. */
-  //   divContainerCards.html('');
-  //   divPagination.html('');
-
-  //   /* Enabling the button again so that the user can click on it. */
-  //   btnSearch.disabled = false;
-  // } else {
-  //   /* Cleaning the container. */
-  //   container.innerHTML = '';
-
-  //   /* Pagination plugin that is being used to paginate the products. */
-  //   divPagination.pagination({
-  //     dataSource: products,
-  //     pageSize: 4,
-  //     callback: function (data, pagination) {
-  //       /* Creating a variable called dataHTML and Add the opening div tag to it. */
-  //       let dataHTML = '<div class="row align-items-center d-flex justify-content-center">';
-
-  //       /* jQuery function to assign all cards to the dataHTML variable */
-  //       $.each(data, function (index, product) {
-  //         dataHTML += `
-  //         <div class="col col-xs-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
-  //           <div class="card" style="width: 14rem; height: 20rem">
-  //             <img
-  //               src="${product.url_image ? product.url_image : '/img/no-image.jpg'}"
-  //               class="card-img-top"
-  //               alt="${product.name}"
-  //               style="width: auto; height: 240px"
-  //             />
-  //             <div class="card-body">
-  //               <h6 class="card-title">${product.name}</h6>
-  //               <p class="card-title">Price: $${product.price}</p>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       `;
-  //       });
-
-  //       /* Adding the closing div tag to the variable dataHTML. */
-  //       dataHTML += '</div>';
-
-  //       /* Replacing the content of the div with the id of 'div-container-cards' with the content of the variable dataHTML. */
-  //       divContainerCards.html(dataHTML);
-
-  //       /* Enabling the button again so that the user can click on it. */
-  //       btnSearch.disabled = false;
-  //     },
-  //   });
-  // }
+  /* Get products and pagination. */
+  pagination(`http://localhost:8080/api/products/search?q=${search}`);
 };
