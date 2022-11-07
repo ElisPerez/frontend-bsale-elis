8d2690bf382482eb86e25f8fd11649cc9e57e7cf
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-11-07 09:03:37 -0500

    padding margin

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ const footerID = document.querySelector('#footer-id');
 
 /* Creating the footer with current Year and a Link to instagram. */
 footerID.innerHTML = `
-      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2);">
         © ${new Date().getFullYear()} Copyright:
         <a class="text-dark" href="https://instagram.com/elisperezmusic" style="text-decoration-line: none;">Elis Antonio Perez</a>
       </div>
